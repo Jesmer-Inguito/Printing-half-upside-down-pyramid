9f9b69ad384edc71c5ddda8e415ce68ca3ad0b72
unknown <you@example.com>
2024-01-26 14:55:13 +0800

    loop created

--- upside_down_pyramid.py
+++ upside_down_pyramid.py
@@ -4,5 +4,7 @@
 rows = int(input("How many rows would you like?: "))
 
 # Create a loop
-
-# Print results
+for i in range(rows, 0, -1):
+    for j in range(0, i):
+        print("*", end=" ")
+    # Print results
